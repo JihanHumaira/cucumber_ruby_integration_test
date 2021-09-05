570eac76b3b9246c04582ea37050ea10cdfef564
fadhli <fadhlimaulidrigmail.com>
2021-09-05 08:05:19 +0700

    first

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'cucumber', '~> 7.0'
 gem 'capybara', '~> 3.35', '>= 3.35.3'
+gem 'selenium-webdriver', '~> 3.142', '>= 3.142.7'
---
+++ features/step_definitions/home_steps.rb
@@ -0,0 +1,31 @@
+
+  Given('user have registered before') do
+    @phone_number='62122333444'
+    @password='password'
+  end
+  
+  When('Users visit login page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('Users fill username text field') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('Users fill password text field') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('Users click login button') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('Users click account icon') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('Users can found their name') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
